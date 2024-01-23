58f778ca8d97e79c0532b37555ca7f5f9d7aa52c
Kieran Murray <KieranMurrayIt@Gmail.com>
2024-01-23 08:36:00 +0000

    first commit

---
+++ CharacterBody2D.gd
@@ -0,0 +1,77 @@
+extends CharacterBody2D
+
+@export var dash_component : DashComponent
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var bounce_learned = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 1200
+var direction : Vector2
+var is_busy :bool = false #Char performing action eg dash
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("test"):
+		get_tree().reload_current_scene()
+		
+	if not is_busy:
+		if velocity.x > 800:
+			velocity.x = 800
+		if velocity.x < -800:
+			velocity.x = -800
+		
+		if velocity.y > 800:
+			velocity.y = 800
+		if velocity.y < -800:
+			velocity.y = -800
+	# Add the gravity.
+	if not is_on_floor() and not is_busy:
+		velocity.y += gravity * delta
+	
+	#get player direction
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	# Handle jump.
+	
+	
+	
+	if Input.is_action_just_pressed("ui_accept"):# and not is_busy: #TODO Celeste wave dash
+		if is_on_floor():
+			jump()
+		else:
+			$PreJumpTimer.start()
+	if is_on_floor() and $PreJumpTimer.time_left > 0:
+		jump()
+		$PreJumpTimer.stop()
+	
+	#Slow player down in air and on ground
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if is_on_floor() and not is_busy:
+		if direction:
+			velocity.x = direction * SPEED # move along ground
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED/10) #friction on floor
+	else:# not on floor
+		if direction and not is_busy: 
+			velocity.x = move_toward(velocity.x, direction * SPEED, SPEED/10)  #friction in air by player
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED/20) # friction in air alone
+	move_and_slide()
+	#elif not is_on_floor() and bounce_learned: # Dashing and on floor
+		#var collision_info = move_and_collide(velocity * delta)
+		#if not collision_info: return
+		#velocity = velocity.bounce(collision_info.get_normal())
+		#velocity.x *= 2
+		#dash_component.can_dash = true
+	#else:
+		#move_and_slide()
+		
+		
+func jump():
+	print("jumping")
+	velocity.y = JUMP_VELOCITY
+func _ready():
+	dash_component.dash_learned = true
+
---
+++ DashComponent.gd
@@ -0,0 +1,83 @@
+extends Node2D
+class_name DashComponent
+
+const UP_RIGHT = Vector2(sqrt(2)/2, -sqrt(2)/2)
+const UP_LEFT = Vector2(-sqrt(2)/2, -sqrt(2)/2)
+const DOWN_RIGHT = Vector2(sqrt(2)/2, sqrt(2)/2)
+const DOWN_LEFT = Vector2(-sqrt(2)/2, sqrt(2)/2)
+const RIGHT = Vector2(1,0)
+const LEFT = Vector2(-1,0)
+const UP = Vector2(0,-1)
+const DOWN = Vector2(0,1)
+const DASH_SPEED = 500
+var dash_learned : bool = false
+var is_dashing : bool = false
+var can_dash : bool = false
+
+var reduce_speed = true
+var dash_direction
+
+@export var char : CharacterBody2D
+
+func dash_listener(direction : Vector2, input : String, is_on_floor : bool):
+	if Input.is_action_just_pressed(input) and can_dash and not is_dashing and not is_on_floor and dash_learned:
+		print("pressed")
+		direction = round_direction(round(rad_to_deg(-direction.angle())))
+		dash_direction = direction
+		if not direction == Vector2(0,0):
+			$DashTimer.start()
+			is_dashing = true
+			char.is_busy = true
+			can_dash = false
+			char.velocity = direction * DASH_SPEED * 2 
+
+func round_direction(deg : float) -> Vector2:
+	if deg == -180:
+		deg = 180
+	if deg < 0: 
+		# Convert to 0 to 360 scale
+		deg = 360 - (deg * -1) 
+	var segment : int = ceil(deg / 22.5)
+	match segment:
+		0, 1:
+			return RIGHT
+		2, 3: 
+			return UP_RIGHT
+		4, 5: 
+			return UP
+		6, 7: 
+			return UP_LEFT
+		8, 9:
+			return LEFT 
+		10, 11: 
+			return DOWN_LEFT
+		12, 13:
+			return DOWN
+		14, 15:
+			return DOWN_RIGHT
+	print("Error: no case deg:", deg, " segment: ", segment)
+	return Vector2(0,0)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if char.is_on_floor():
+		can_dash = true
+		$DashTimer.start(0.25)
+	dash_listener(char.direction, "dash", char.is_on_floor())
+	
+
+func _on_dash_timer_timeout():
+	is_dashing = false
+	char.is_busy = false
+	
+func cancel_dash():
+	is_dashing = false
+	char.is_busy = false
+	$DashTimer.stop()
